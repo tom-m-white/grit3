c686af47d6525869eb63063efc055b09d754111d
Tom White <whitet1457@gmail.com>
2026-05-30 20:47:40 -0400

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/grit3/",
   plugins: [react()],
   build: {
     rollupOptions: {
